9911e7b179f60dada00bf7c7fb55373527b7ec7e
slashDaytona <161556890+arisinha@users.noreply.github.com>
2025-06-11 23:54:52 +0000

    X

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Ivan Reyes Segoviano",
+  name: "Nelson Ivan Reyes Segoviano",
   title: "Software Engineer",
   email: "nelson_segoviano@hotmail.com",
   gitHub: "arisinha",
