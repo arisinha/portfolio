d50e5000114d11b50ee20e0d1db198731e238ec5
slashDaytona <161556890+arisinha@users.noreply.github.com>
2025-06-11 23:27:22 +0000

    X

---
+++ src/Components/Education.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Education = () => {
+  return (
+    <section className="padding" id="education">
+      <h2 style={{ textAlign: "center" }}>Education</h2>
+      <div style={{ display: "flex", flexDirection: "column", paddingTop: "3rem" }}>
+        <div style={{ marginBottom: "2rem" }}>
+          <h3>Universidad Autonoma de Occidente</h3>
+          <p>Software Engineering Student</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
